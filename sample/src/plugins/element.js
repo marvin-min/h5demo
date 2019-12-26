9ce0b3820088f0bd16fac2f8b4bf599560196cb9
Marvin.Min <marvin.min@activenetwork.com>
2019-12-26 21:57:01 +0800

    add axios

---
+++ sample/src/plugins/element.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+//https://element.eleme.io/#/zh-CN/component/quickstart
+import { Button,Link } from "element-ui";
+Vue.use(Button);
+Vue.use(Link);
--- sample/vue.config.js
+++ sample/vue.config.js
@@ -1,6 +1,6 @@
-module.exports={
-    devServer: {
-        open: true,
-        port: 8080,
-    }
-}
+module.exports = {
+  devServer: {
+    open: true,
+    port: 8080
+  }
+};
