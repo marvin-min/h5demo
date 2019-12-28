2204ef8312374d24d5b9350a779d4a8cf7a48b7e
Marvin.Min <marvin.min@activenetwork.com>
2019-12-28 13:08:13 +0800

    add basic layout

--- sample/src/components/Home.vue
+++ sample/src/components/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="home">
-    <el-button type="info" @click="logout">退出登录</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <el-button type="info" @click="logout">退出登录</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -20,6 +29,17 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.home {
+.home-container {
+  width: 100%;
+  height: 100%;
+  .el-header {
+    background-color: #b2c0d1;
+  }
+  .el-aside {
+    background-color: #d3dce6;
+  }
+  .el-footer {
+    background-color: #e9eef3;
+  }
 }
 </style>
--- sample/src/plugins/element.js
+++ sample/src/plugins/element.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 //https://element.eleme.io/#/zh-CN/component/quickstart
-import { Button, Link, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Link,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Footer,
+  Main
+} from 'element-ui'
 Vue.use(Button)
 Vue.use(Link)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Footer)
+Vue.use(Main)
 Vue.prototype.$msg = Message
